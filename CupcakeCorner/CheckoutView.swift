259b1a9bae47b686857ce0740645ceb866dedf47
aajuvale <ahmed.juvale@gmail.com>
2025-08-06 10:29:35 -0700

    feat: implemented address validation

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,16 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Ahmed Juvale on 8/6/25.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
