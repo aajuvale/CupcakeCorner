14bdd4257e3cc5e9680edbc3b1ffa9425408c056
aajuvale <ahmed.juvale@gmail.com>
2025-08-06 10:20:56 -0700

    feat: implemented basic order details inputs

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -5,55 +5,42 @@
 //  Created by Ahmed Juvale on 8/5/25.
 //
 
-import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
     @State
-    private var engine: CHHapticEngine?
+    private var order = Order()
 
     var body: some View {
-        Button("Play Haptics", action: complexSuccess)
-            .onAppear(perform: prepareHaptics)
-    }
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
 
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
 
-        do {
-            engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("There was an error creating the engine: \(error.localizedDescription)")
-        }
-    }
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled)
 
-    func complexSuccess() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
 
-        var events = [CHHapticEvent]()
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
 
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-            events.append(event)
-        }
-
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1 - Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1 - Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
-            events.append(event)
-        }
-
-
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription)")
+                Section {
+                    NavigationLink("Delivery Details") {
+                        AddressView(order: order)
+                    }
+                }
+            }
+            .navigationTitle("Cupcake Corner")
         }
     }
 }
